7f7e752d3c7ef40e0cce27c882f1ddf01d4275df
Sunil R <sunil.1si19et054@gmail.com>
2026-05-30 21:51:56 +0530

    Updated scheduler

--- CustomRTOS/RTOS/scheduler.c
+++ CustomRTOS/RTOS/scheduler.c
@@ -1,13 +1,51 @@
 #include "rtos.h"
-    __asm volatile(
-        "PUSH {LR}             \n"
-        "BL get_psp_value      \n"
-        "MSR PSP,R0            \n"
-        "POP {LR}              \n"
-        "MOV R0,#0x02          \n"
-        "MSR CONTROL,R0        \n"
-        "BX LR                 \n"
-    );
+
+extern TCB_t userTasks[MAX_TASKS];
+extern uint8_t currentTask;
+extern uint32_t globalTickCount;
+
+void init_scheduler_stack(uint32_t schedTopOfStack)
+{
+    __asm volatile("MSR MSP,%0" : : "r"(schedTopOfStack) : );
+}
+
+void init_tasks_stack(void)
+{
+    uint32_t *psp;
+
+    psp = (uint32_t*)T1_STACK_START;
+
+    for(int i = 0; i < MAX_TASKS; i++)
+    {
+        userTasks[i].currentState = TASK_READY_STATE;
+
+        psp--;
+        *psp = DUMMY_XPSR;
+
+        psp--;
+        *psp = (uint32_t)userTasks[i].taskHandler;
+
+        psp--;
+        *psp = 0xFFFFFFFD;
+
+        /* R12,R3,R2,R1,R0 */
+        for(int j = 0; j < 5; j++)
+        {
+            psp--;
+            *psp = 0;
+        }
+
+        /* R4-R11 */
+        for(int j = 0; j < 8; j++)
+        {
+            psp--;
+            *psp = 0;
+        }
+
+        userTasks[i].pspValue = (uint32_t)psp;
+
+        psp = psp - SIZE_TASK_STACK;
+    }
 }
 
 uint32_t get_psp_value(void)
@@ -20,6 +58,19 @@ void save_psp_value(uint32_t currentPspValue)
     userTasks[currentTask].pspValue = currentPspValue;
 }
 
+void switch_sp_to_psp(void)
+{
+    __asm volatile(
+        "PUSH {LR}          \n"
+        "BL get_psp_value   \n"
+        "MSR PSP,R0         \n"
+        "POP {LR}           \n"
+        "MOV R0,#0x02       \n"
+        "MSR CONTROL,R0     \n"
+        "BX LR              \n"
+    );
+}
+
 void update_next_task(void)
 {
     uint8_t state = TASK_BLOCKED_STATE;
@@ -27,6 +78,7 @@ void update_next_task(void)
     for(uint32_t i = 0; i < MAX_TASKS; i++)
     {
         currentTask++;
+
         currentTask %= MAX_TASKS;
 
         state = userTasks[currentTask].currentState;
@@ -44,8 +96,11 @@ void task_delay(uint32_t tickCount)
 
     if(currentTask)
     {
-        userTasks[currentTask].blockCount = globalTickCount + tickCount;
-        userTasks[currentTask].currentState = TASK_BLOCKED_STATE;
+        userTasks[currentTask].blockCount =
+                globalTickCount + tickCount;
+
+        userTasks[currentTask].currentState =
+                TASK_BLOCKED_STATE;
 
         schedule();
     }
@@ -59,9 +114,10 @@ void unblock_tasks(void)
     {
         if(userTasks[i].currentState != TASK_READY_STATE)
         {
-            if(userTasks[i].blockCount == globalTickCount)
+            if(userTasks[i].blockCount <= globalTickCount)
             {
-                userTasks[i].currentState = TASK_READY_STATE;
+                userTasks[i].currentState =
+                        TASK_READY_STATE;
             }
         }
     }
